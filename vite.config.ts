861b4cc23dfe67d6f3561e27f7cda4cd68bbbaf7
rag-nar1 <ragnar1242003@gmail.com>
2025-03-24 16:26:26 +0200

    Update base URL to match new repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tcpDuckdbWeb/',
+  base: '/Duck-it-website/',
   resolve: {
     alias: {
       '@': '/src',
